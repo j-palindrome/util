decafb51ccffed6eb7f757efa3b1712c59fbfa61
Joshua Tazman Reinier <jreinier@oberlin.edu>
2024-12-17 20:01:29 -0800

    update headers

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,25 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://www.jpalindrome.com https://jpalindrome.com '
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+          }
+        ]
+      }
+    ]
   }
 }
 
