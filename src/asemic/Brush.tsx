84df24ad4395d6190e506326a0ad75f49def857f
Joshua Tazman Reinier <jreinier@oberlin.edu>
2024-12-29 21:00:39 -0800

    Revert "texture calls badly"
    This reverts commit 22dba2c4ef4386634a2f16b23bf5f8b4b5cdf4b2.

--- src/asemic/Brush.tsx
+++ src/asemic/Brush.tsx
@@ -1,22 +1,38 @@
-import { extend, useThree } from '@react-three/fiber'
-import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react'
+import { isEqual, now } from 'lodash'
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState
+} from 'react'
 import * as THREE from 'three'
 import { Vector2 } from 'three'
 import {
   atan2,
   attribute,
+  cos,
   float,
   Fn,
   If,
   instanceIndex,
   ivec2,
+  log,
+  mat2,
   mix,
+  PI2,
+  pow,
+  sampler,
   screenSize,
+  select,
+  sin,
   texture,
   textureLoad,
   textureStore,
   uniform,
   uniformArray,
+  uvec2,
   varying,
   varyingProperty,
   vec2,
@@ -30,8 +46,11 @@ import {
   StorageTexture,
   WebGPURenderer
 } from 'three/webgpu'
-import { bezierPoint, lineTangent, multiBezierProgress } from '../tsl/curves'
+import { useEventListener, useInterval } from '../dom'
 import Builder from './Builder'
+import { extend, useFrame, useThree } from '@react-three/fiber'
+import { updateInstanceAttribute } from '../three'
+import { bezierPoint, lineTangent, multiBezierProgress } from '../tsl/curves'
 
 type VectorList = [number, number]
 type Vector3List = [number, number, number]
@@ -58,7 +77,6 @@ export default function Brush({
   )
 
   const [lastData, setLastData] = useState(keyframes.reInitialize(resolution))
-
   const colorTexRef = useRef(lastData.colorTex)
   colorTexRef.current = lastData.colorTex
   const thicknessTexRef = useRef(lastData.thicknessTex)
@@ -71,16 +89,16 @@ export default function Brush({
     lastData.dimensions.x,
     lastData.dimensions.y
   )
-  storageTexture.minFilter = storageTexture.magFilter = THREE.LinearFilter
   storageTexture.type = THREE.FloatType
+  storageTexture.minFilter = storageTexture.magFilter = THREE.NearestFilter
   const dimensionsU = uniform(lastData.dimensions, 'vec2')
 
   // @ts-ignore
   const textureLoadFix = wgslFn<{
-    uv: ReturnType<typeof ivec2>
-    tex: ReturnType<typeof texture>
+    tex: StorageTexture
+    uv: ReturnType<typeof vec2>
   }>(/*wgsl*/ `
-    fn loadTexture(uv: vec2<i32>, tex: texture_2d<f32>) -> vec4<f32> {
+    fn loadTexture(tex: texture_2d<f32>, uv: vec2<f32>) -> vec4<f32> {
       return textureLoad(tex, uv, 0);
     }`)
 
@@ -129,10 +147,6 @@ export default function Brush({
   }, [])
 
   useMemo(() => {
-    //  nodeVar1 = textureLoad( nodeUniform1, vec2u( tsl_coord_clampS_clampT( ( object.nodeUniform2 * vec3<f32>( nodeVarying1, 1.0 ) ).xy ) * vec2f( nodeVar2 ) ) ); <-- this Goes Away without filtering being set to nearest. Otherwise it goes to a bogus textureLoad function which I can't seem to replace.
-    // this simply isn't working. I've tried everything.
-    // Disable the filtering...
-    // const storageTexNode = uniform(storageTexture, 'texture_2d<f32>')
     const rotation = float(0).toVar('rotation')
     const thickness = float(10).toVar('thickness')
     const t = attribute('t', 'vec2')
@@ -141,96 +155,93 @@ export default function Brush({
       lastData.groups[0].controlPointCounts as any,
       'int'
     )
-    const storageTexNode = texture(storageTexture)
-    const main = Fn(() =>
-      // { storageTex }: { storageTex: THREE.Texture }
-      {
-        // TODO: fix instancing...
-        const curveProgress = t.y.add(0.5).div(dimensionsU.y)
-        const controlPointsCount = controlPointCounts.element(t.y)
 
-        let point = {
-          position: vec2(0, 0).toVar('thisPosition'),
-          rotation: float(0).toVar('thisRotation')
-        }
+    const main = Fn(({ storageTex }: { storageTex: THREE.Texture }) => {
+      // TODO: fix instancing...
+      const curveProgress = t.y.add(0.5).div(dimensionsU.y)
+      const controlPointsCount = controlPointCounts.element(t.y)
 
-        If(controlPointsCount.equal(2), () => {
-          const p0 = textureLoadFix({
-            tex: storageTexNode,
-            uv: ivec2(0, t.y)
-          }).xy.toVar('p0')
-          const p1 = textureLoadFix({
-            tex: storageTexNode,
-            uv: ivec2(1, t.y)
-          }).xy.toVar('p1')
-          const progressPoint = mix(p0, p1, t.x)
-          point.position.assign(progressPoint)
-          const rotation = lineTangent(p0, p1, aspectRatio)
-          point.rotation.assign(atan2(rotation.y, rotation.x))
-          const textureVector = vec2(
-            t.x.add(0.5).div(dimensionsU.x),
-            curveProgress
-          )
-          varyingProperty('vec4', 'colorV').assign(
-            texture(colorTexRef.current, textureVector)
-          )
-          thickness.assign(texture(thicknessTexRef.current, textureVector))
-        }).Else(() => {
-          const pointProgress = multiBezierProgress({
-            t: t.x,
-            controlPointsCount
-          })
-          const tt = vec2(
-            t.x.mul(controlPointsCount.sub(1)).add(0.5).div(dimensionsU.x),
-            curveProgress
-          )
-          const p0 = textureLoadFix({
-            tex: storageTexNode,
-            uv: ivec2(pointProgress.x, t.y)
-          }).xy.toVar('p0')
-          const p1 = textureLoadFix({
-            tex: storageTexNode,
-            uv: ivec2(pointProgress.x.add(1), t.y)
-          }).xy.toVar('p1')
-          const p2 = textureLoadFix({
-            tex: storageTexNode,
-            uv: ivec2(pointProgress.x.add(2), t.y)
-          }).xy.toVar('p2')
-          const strength = textureLoadFix({
-            tex: storageTexNode,
-            uv: ivec2(pointProgress.x.add(1), t.y)
-          }).z.toVar('strength')
-
-          varyingProperty('vec4', 'colorV').assign(
-            texture(colorTexRef.current, tt)
-          )
-          thickness.assign(texture(thicknessTexRef.current, tt))
-
-          If(pointProgress.x.greaterThan(float(0)), () => {
-            p0.assign(mix(p0, p1, float(0.5)))
-          })
-          If(pointProgress.x.lessThan(float(controlPointsCount).sub(3)), () => {
-            p2.assign(mix(p1, p2, 0.5))
-          })
-          const thisPoint = bezierPoint({
-            t: pointProgress.y,
-            p0,
-            p1,
-            p2,
-            strength,
-            aspectRatio
-          })
-          // point.position.assign(p0)
-          point.position.assign(thisPoint.position)
-          point.rotation.assign(thisPoint.rotation)
-        })
-
-        rotation.assign(vec3(point.rotation, 0, 0))
-        return vec4(point.position, 0, 1)
+      let point = {
+        position: vec2(0, 0).toVar('thisPosition'),
+        rotation: float(0).toVar('thisRotation')
       }
-    )
 
-    material.positionNode = main()
+      If(controlPointsCount.equal(2), () => {
+        const p0 = textureLoadFix({
+          tex: storageTex,
+          uv: ivec2(0, t.y)
+        }).xy.toVar('p0')
+        const p1 = textureLoadFix({
+          tex: storageTex,
+          uv: ivec2(1, t.y)
+        }).xy.toVar('p1')
+        const progressPoint = mix(p0, p1, t.x)
+        point.position.assign(progressPoint)
+        const rotation = lineTangent(p0, p1, aspectRatio)
+        point.rotation.assign(atan2(rotation.y, rotation.x))
+        const textureVector = vec2(
+          t.x.add(0.5).div(dimensionsU.x),
+          curveProgress
+        )
+        varyingProperty('vec4', 'colorV').assign(
+          texture(colorTexRef.current, textureVector)
+        )
+        thickness.assign(texture(thicknessTexRef.current, textureVector))
+      }).Else(() => {
+        const pointProgress = multiBezierProgress({
+          t: t.x,
+          controlPointsCount
+        })
+        const tt = vec2(
+          t.x.mul(controlPointsCount.sub(1)).add(0.5).div(dimensionsU.x),
+          curveProgress
+        )
+        const p0 = textureLoadFix({
+          tex: storageTex,
+          uv: ivec2(pointProgress.x, t.y)
+        }).xy.toVar('p0')
+        const p1 = textureLoadFix({
+          tex: storageTex,
+          uv: ivec2(pointProgress.x.add(1), t.y)
+        }).xy.toVar('p1')
+        const p2 = textureLoadFix({
+          tex: storageTex,
+          uv: ivec2(pointProgress.x.add(2), t.y)
+        }).xy.toVar('p2')
+        const strength = textureLoadFix({
+          tex: storageTex,
+          uv: ivec2(pointProgress.x.add(1), t.y)
+        }).z.toVar('strength')
+
+        varyingProperty('vec4', 'colorV').assign(
+          texture(colorTexRef.current, tt)
+        )
+        thickness.assign(texture(thicknessTexRef.current, tt))
+
+        If(pointProgress.x.greaterThan(float(0)), () => {
+          p0.assign(mix(p0, p1, float(0.5)))
+        })
+        If(pointProgress.x.lessThan(float(controlPointsCount).sub(3)), () => {
+          p2.assign(mix(p1, p2, 0.5))
+        })
+        const thisPoint = bezierPoint({
+          t: pointProgress.y,
+          p0,
+          p1,
+          p2,
+          strength,
+          aspectRatio
+        })
+        // point.position.assign(p0)
+        point.position.assign(thisPoint.position)
+        point.rotation.assign(thisPoint.rotation)
+      })
+
+      rotation.assign(vec3(point.rotation, 0, 0))
+      return vec4(point.position, 0, 1)
+    })
+
+    material.positionNode = main({ storageTex: storageTexture })
     material.rotationNode = rotation
     const pixel = float(1.414).mul(2).div(resolution.length())
     material.scaleNode = vec2(thickness.mul(pixel), pixel)
@@ -238,7 +249,6 @@ export default function Brush({
     const vDirection = varying(vec4(), 'colorV')
     material.colorNode = vDirection
     material.needsUpdate = true
-    storageTexture.needsUpdate = true
   }, [lastData])
 
   const instanceCount = Math.floor(
@@ -254,7 +264,7 @@ export default function Brush({
   useLayoutEffect(() => {
     // ~10ms
     if (!meshRef.current) return
-    // storageTexture.minFilter = storageTexture.magFilter = THREE.NearestFilter
+
     meshRef.current.children.forEach(child => {
       const c = child as THREE.InstancedMesh<
         THREE.PlaneGeometry,
