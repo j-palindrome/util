54ec418c566e3fb6c8b8f4b89f67f38b8803f619
Joshua Tazman Reinier <jreinier@oberlin.edu>
2025-01-18 12:32:19 -0500

    encapsulate curves

--- src/asemic/MeshBrush.tsx
+++ src/asemic/MeshBrush.tsx
@@ -35,7 +35,7 @@ import {
 import { bezierPoint } from '../tsl/curves'
 import { textureLoadFix } from '../tsl/utility'
 import { GroupBuilder } from './Builder'
-import { createTexture, packToTexture } from './util/packTexture'
+import { useControlPoints } from './util/packTexture'
 
 type VectorList = [number, number]
 type Vector3List = [number, number, number]
@@ -62,7 +62,6 @@ export default function MeshBrush({
 }: {
   builder: GroupBuilder<'line'>
 }) {
-  const [rendering, setRendering] = useState(true)
   // @ts-ignore
   const gl = useThree(({ gl }) => gl as WebGPURenderer)
   const resolution = new Vector2()
@@ -72,344 +71,49 @@ export default function MeshBrush({
     (builder.settings.maxLength * width) / (builder.settings.spacing * 5)
   )
 
-  const {
-    mesh,
-    material,
-    geometry,
-    curvePositionTex,
-    curveColorTex,
-    controlPointCounts
-  } = useMemo(() => {
-    const type: string = 'line'
-    if (type === 'mesh') {
-      const geometry = new THREE.BufferGeometry()
-      geometry.setAttribute('position', new THREE.Float32BufferAttribute([], 3))
-      const indexGuide = [0, 1, 2, 1, 2, 3]
+  const { mesh, material, geometry } = useMemo(() => {
+    const geometry = new THREE.BufferGeometry()
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute([], 3))
+    const indexGuide = [0, 1, 2, 1, 2, 3]
 
-      let currentIndex = 0
-      const indexes: number[] = []
-      for (let i = 0; i < builder.settings.maxCurves; i++) {
-        for (let i = 0; i < verticesPerCurve - 1; i++) {
-          indexes.push(...indexGuide.map(x => x + currentIndex))
-          currentIndex += 2
-        }
+    let currentIndex = 0
+    const indexes: number[] = []
+    for (let i = 0; i < builder.settings.maxCurves; i++) {
+      for (let i = 0; i < verticesPerCurve - 1; i++) {
+        indexes.push(...indexGuide.map(x => x + currentIndex))
         currentIndex += 2
       }
+      currentIndex += 2
+    }
+    geometry.setIndex(indexes)
+    const material = new MeshBasicNodeMaterial({
+      transparent: true,
+      depthWrite: false,
+      blending: THREE.NormalBlending,
+      side: THREE.DoubleSide,
+      color: 'white'
+    })
+    material.mrtNode = builder.settings.renderTargets
+    const mesh = new THREE.Mesh(geometry, material)
 
-      geometry.setIndex(indexes)
-
-      const material = new MeshBasicNodeMaterial({
-        transparent: true,
-        depthWrite: false,
-        blending: THREE.NormalBlending,
-        side: THREE.DoubleSide,
-        color: 'white'
-      })
-
-      material.mrtNode = builder.settings.renderTargets
-
-      const mesh = new THREE.Mesh(geometry, material)
-
-      const curvePositionTex = new StorageTexture(
-        builder.settings.maxPoints,
-        builder.settings.maxCurves
-      )
-      curvePositionTex.type = THREE.FloatType
-      const curveColorTex = new StorageTexture(
-        builder.settings.maxPoints,
-        builder.settings.maxCurves
-      )
-      const controlPointCounts = uniformArray(
-        builder.curves.map(x => x.length),
-        'int'
-      )
-      return {
-        mesh,
-        material,
-        geometry,
-        curvePositionTex,
-        curveColorTex,
-        controlPointCounts
-      }
-    } else {
-      const geometry = new THREE.BufferGeometry()
-      geometry.setAttribute('position', new THREE.Float32BufferAttribute([], 3))
-      const indexGuide = [0, 1, 2, 1, 2, 3]
-
-      let currentIndex = 0
-      const indexes: number[] = []
-      for (let i = 0; i < builder.settings.maxCurves; i++) {
-        for (let i = 0; i < verticesPerCurve - 1; i++) {
-          indexes.push(...indexGuide.map(x => x + currentIndex))
-          currentIndex += 2
-        }
-        currentIndex += 2
-      }
-
-      geometry.setIndex(indexes)
-
-      const material = new MeshBasicNodeMaterial({
-        transparent: true,
-        depthWrite: false,
-        blending: THREE.NormalBlending,
-        side: THREE.DoubleSide,
-        color: 'white'
-      })
-
-      material.mrtNode = builder.settings.renderTargets
-
-      const mesh = new THREE.Mesh(geometry, material)
-
-      const curvePositionTex = new StorageTexture(
-        builder.settings.maxPoints,
-        builder.settings.maxCurves
-      )
-      curvePositionTex.type = THREE.FloatType
-      const curveColorTex = new StorageTexture(
-        builder.settings.maxPoints,
-        builder.settings.maxCurves
-      )
-      const controlPointCounts = uniformArray(
-        builder.curves.map(x => x.length),
-        'int'
-      )
-      return {
-        mesh,
-        material,
-        geometry,
-        curvePositionTex,
-        curveColorTex,
-        controlPointCounts
-      }
+    return {
+      mesh,
+      material,
+      geometry
     }
   }, [builder])
 
-  const {
-    advanceControlPoints,
-    curvePositionLoadU,
-    curveColorLoadU,
-    lastCurvePositionLoadU,
-    lastCurveColorLoadU
-  } = useMemo(() => {
-    const { positions, colors } = packToTexture(
-      new Vector2(builder.settings.maxPoints, builder.settings.maxCurves),
-      builder.curves
-    )
-    const positionTex = createTexture(
-      positions,
-      builder.settings.maxPoints,
-      builder.settings.maxCurves
-    )
-    const colorTex = createTexture(
-      colors,
-      builder.settings.maxPoints,
-      builder.settings.maxCurves
-    )
+  const { getBezier } = useControlPoints(builder)
 
-    const dimensionsU = uniform(
-      new Vector2(builder.settings.maxPoints, builder.settings.maxCurves),
-      'vec2'
-    )
-    const aspectRatio = screenSize.div(screenSize.x).toVar('screenSize')
-
-    const pointI = instanceIndex.modInt(builder.settings.maxPoints)
-    const curveI = instanceIndex.div(builder.settings.maxPoints)
-    const curvePositionLoadU = textureLoad(positionTex, vec2(pointI, curveI))
-    const curveColorLoadU = textureLoad(colorTex, vec2(pointI, curveI))
-    const lastCurvePositionLoadU = textureLoad(
-      positionTex,
-      vec2(pointI, curveI)
-    )
-    const lastCurveColorLoadU = textureLoad(colorTex, vec2(pointI, curveI))
-
-    const advanceControlPoints = Fn(() => {
-      const textureVector = vec2(
-        pointI.toFloat().div(controlPointCounts.element(curveI)),
-        curveI.toFloat().div(builder.settings.maxCurves)
-      )
-      const info = {
-        pointUV: textureVector,
-        aspectRatio: aspectRatio.y,
-        settings: builder.settings
-      }
-      const point = builder.settings.curveVert(vec4(curvePositionLoadU), {
-        ...info,
-        lastPosition: vec4(lastCurvePositionLoadU)
-      })
-      textureStore(curvePositionTex, vec2(pointI, curveI), point).toWriteOnly()
-
-      const color = builder.settings.curveFrag(vec4(curveColorLoadU), {
-        ...info,
-        lastColor: vec4(lastCurveColorLoadU)
-      })
-      return textureStore(
-        curveColorTex,
-        vec2(pointI, curveI),
-        color
-      ).toWriteOnly()
-    })().compute(
-      builder.settings.maxPoints * builder.settings.maxCurves,
-      undefined as any
-    )
-
-    // const getBezier = (pointProgress, curveProgress, controlPointsCount) => {
-    //   const p2 = textureLoadFix(
-    //     curvePositionTexU,
-    //     ivec2(pointProgress.add(2), curveProgress)
-    //   ).xy.toVar('p2')
-    //   const p0 = textureLoadFix(
-    //     curvePositionTexU,
-    //     ivec2(pointProgress, curveProgress)
-    //   ).xy.toVar('p0')
-    //   const p1 = textureLoadFix(
-    //     curvePositionTexU,
-    //     ivec2(pointProgress.add(1), curveProgress)
-    //   ).xy.toVar('p1')
-
-    //   If(pointProgress.greaterThan(float(1)), () => {
-    //     p0.assign(mix(p0, p1, float(0.5)))
-    //   })
-    //   If(pointProgress.lessThan(float(controlPointsCount).sub(3)), () => {
-    //     p2.assign(mix(p1, p2, 0.5))
-    //   })
-    //   const strength = textureLoadFix(
-    //     curvePositionTexU,
-    //     ivec2(pointProgress.add(1), curveProgress)
-    //   ).z.toVar('strength')
-    //   return bezierPoint({
-    //     t: pointProgress.fract(),
-    //     p0,
-    //     p1,
-    //     p2,
-    //     strength
-    //   })
-    // }
-
+  useMemo(() => {
+    const position = vec2().toVar('thisPosition')
     const rotation = float(0).toVar('rotation')
     const thickness = float(0).toVar('thickness')
-    const curvePositionTexU = texture(curvePositionTex)
+    const color = varyingProperty('vec4', 'color')
 
     const main = Fn(() => {
-      const curveIndex = vertexIndex
-        .div(2)
-        .div(verticesPerCurve)
-        .toVar('curveIndex')
-      const controlPointsCount = controlPointCounts.element(curveIndex)
-      const subdivisions = select(
-        controlPointsCount.equal(2),
-        1,
-        controlPointsCount.sub(2)
-      ).toVar('subdivisions')
-      const t = vec2(
-        vertexIndex
-          .div(2)
-          .toFloat()
-          .mod(verticesPerCurve)
-          .div(verticesPerCurve - 0.99)
-          .mul(subdivisions),
-        curveIndex
-      )
+      getBezier(vertexIndex, position, rotation, thickness, color)
 
-      const position = vec2().toVar('thisPosition')
-
-      If(t.x.equal(-1), () => {
-        varyingProperty('vec4', 'colorV').assign(vec4(0, 0, 0, 0))
-      }).Else(() => {
-        If(controlPointsCount.equal(2), () => {
-          const p0 = textureLoadFix(curvePositionTexU, ivec2(0, t.y)).xy
-          const p1 = textureLoadFix(curvePositionTexU, ivec2(1, t.y)).xy
-          If(p1.x.equal(-1111), () => {
-            varyingProperty('vec4', 'colorV').assign(vec4(0, 0, 0, 0))
-          }).Else(() => {
-            const pointUV = vec2(t.x, t.y.div(dimensionsU.y))
-            const info = {
-              pointUV,
-              aspectRatio: aspectRatio.y,
-              settings: builder.settings
-            }
-            const progressPoint = mix(p0, p1, t.x)
-            // const rotate = lineTangent(p0, p1).toVar('rotate')
-            const textureVector = vec2(
-              t.x.add(0.5).div(dimensionsU.x),
-              t.y.add(0.5).div(dimensionsU.y)
-            )
-            varyingProperty('vec4', 'colorV').assign(
-              builder.settings.pointFrag(
-                vec4(texture(curveColorTex, textureVector)),
-                info
-              )
-            )
-            thickness.assign(texture(curvePositionTex, textureVector).w)
-            position.assign(progressPoint)
-
-            rotation.assign(atan(p1.sub(p0).y, p1.sub(p0).x).add(PI2.mul(0.25)))
-          })
-        }).Else(() => {
-          const pointProgress = t.x
-          const p2 = textureLoadFix(
-            curvePositionTexU,
-            ivec2(pointProgress.add(2), t.y)
-          ).xy.toVar('p2')
-
-          If(p2.x.equal(-1111), () => {
-            varyingProperty('vec4', 'colorV').assign(vec4(0, 0, 0, 0))
-          }).Else(() => {
-            const p0 = textureLoadFix(
-              curvePositionTexU,
-              ivec2(pointProgress, t.y)
-            ).xy.toVar('p0')
-            const p1 = textureLoadFix(
-              curvePositionTexU,
-              ivec2(pointProgress.add(1), t.y)
-            ).xy.toVar('p1')
-
-            If(pointProgress.greaterThan(float(1)), () => {
-              p0.assign(mix(p0, p1, float(0.5)))
-            })
-            If(pointProgress.lessThan(float(controlPointsCount).sub(3)), () => {
-              p2.assign(mix(p1, p2, 0.5))
-            })
-            const strength = textureLoadFix(
-              curvePositionTexU,
-              ivec2(pointProgress.add(1), t.y)
-            ).z.toVar('strength')
-            const thisPoint = bezierPoint({
-              t: pointProgress.fract(),
-              p0,
-              p1,
-              p2,
-              strength
-            })
-            const tt = vec2(
-              // 2 points: 0.5-1.5
-              pointProgress
-                .div(controlPointsCount.sub(2))
-                .mul(controlPointsCount.sub(1))
-                .add(0.5)
-                .div(dimensionsU.x),
-              t.y.add(0.5).div(dimensionsU.y)
-            )
-            const pointUV = vec2(
-              pointProgress.div(controlPointsCount.sub(2)),
-              t.y.div(dimensionsU.y)
-            )
-            const info = {
-              pointUV,
-              aspectRatio: aspectRatio.y,
-              settings: builder.settings
-            }
-            varyingProperty('vec4', 'colorV').assign(
-              builder.settings.pointFrag(vec4(texture(curveColorTex, tt)), info)
-            )
-            thickness.assign(texture(curvePositionTex, tt).w)
-            position.assign(
-              builder.settings.pointVert(thisPoint.position, info)
-            )
-            rotation.assign(thisPoint.rotation)
-          })
-        })
-      })
-      thickness.divAssign(screenSize.x)
       position.addAssign(
         rotateUV(
           vec2(
@@ -425,93 +129,17 @@ export default function MeshBrush({
     })
 
     material.positionNode = main()
-
-    const colorV = varying(vec4(), 'colorV')
-    material.colorNode = colorV
+    material.colorNode = varying(vec4(), 'color')
     material.needsUpdate = true
-
-    return {
-      advanceControlPoints,
-      curvePositionLoadU,
-      curveColorLoadU,
-      lastCurvePositionLoadU,
-      lastCurveColorLoadU
-    }
-  }, [builder])
-
-  let updating: number
-
-  const reInitialize = () => {
-    builder.reInitialize(resolution)
-    for (let i = 0; i < builder.settings.maxCurves; i++) {
-      controlPointCounts.array[i] = builder.curves[i].length
-    }
-
-    lastCurvePositionLoadU.value = curvePositionLoadU.value
-    lastCurveColorLoadU.value = curveColorLoadU.value
-    const newData = packToTexture(
-      new Vector2(builder.settings.maxPoints, builder.settings.maxCurves),
-      builder.curves
-    )
-
-    const loadPositions = curvePositionLoadU.value.source.data
-      .data as Float32Array
-    loadPositions.set(newData.positions)
-    const loadColors = curveColorLoadU.value.source.data.data as Float32Array
-    loadColors.set(newData.colors)
-    curvePositionLoadU.value.needsUpdate = true
-    curveColorLoadU.value.needsUpdate = true
-
-    gl.computeAsync(advanceControlPoints)
-  }
-
-  const nextTime = useRef<number>(builder.settings.start / 1000)
-
-  useFrame(state => {
-    if (state.clock.elapsedTime > nextTime.current) {
-      if (builder.settings.recalculate) {
-        const r = builder.settings.recalculate
-        nextTime.current =
-          typeof r === 'boolean'
-            ? nextTime.current + 1 / 60
-            : typeof r === 'number'
-              ? nextTime.current + r / 1000
-              : nextTime.current + r(nextTime.current * 1000) / 1000
-        reInitialize()
-      }
-    }
-  })
-
-  const update = () => {
-    gl.computeAsync(advanceControlPoints)
-    material.needsUpdate = true
-    updating = requestAnimationFrame(update)
-  }
-
-  useEffect(() => {
-    console.log('reinit')
-
-    reInitialize()
-    if (rendering && builder.settings.update) {
-      updating = requestAnimationFrame(update)
-    }
-    return () => {
-      cancelAnimationFrame(updating)
-    }
   }, [builder])
 
   const scene = useThree(({ scene }) => scene)
   useEffect(() => {
-    if (rendering) {
-      scene.add(mesh)
-    }
+    scene.add(mesh)
     return () => {
       scene.remove(mesh)
-      // mesh.dispose()
       material.dispose()
       geometry.dispose()
-      curvePositionTex.dispose()
-      curveColorTex.dispose()
     }
   }, [builder])
 
--- src/asemic/util/packTexture.ts
+++ src/asemic/util/packTexture.ts
@@ -1,6 +1,7 @@
+import { useFrame, useThree } from '@react-three/fiber'
 import { range } from 'lodash'
+import { useEffect, useMemo, useRef } from 'react'
 import {
-  AnyPixelFormat,
   ClampToEdgeWrapping,
   DataTexture,
   FloatType,
@@ -8,60 +9,327 @@ import {
   MagnificationTextureFilter,
   NearestFilter,
   PixelFormat,
-  RedFormat,
   RGBAFormat,
   Vector2
 } from 'three'
+import { Fn } from 'three/src/nodes/TSL.js'
+import {
+  atan,
+  float,
+  If,
+  instanceIndex,
+  ivec2,
+  mix,
+  PI2,
+  screenSize,
+  select,
+  texture,
+  textureLoad,
+  textureStore,
+  uniformArray,
+  vec2,
+  vec4
+} from 'three/tsl'
+import { StorageTexture, WebGPURenderer } from 'three/webgpu'
 import { GroupBuilder } from '../Builder'
+import { textureLoadFix } from '../../tsl/utility'
+import { bezierPoint } from '../../tsl/curves'
 
-export const createTexture = (
-  array: Float32Array,
-  width: number,
-  height: number,
-  {
-    filter = NearestFilter,
-    format = RGBAFormat
-  }: { filter?: MagnificationTextureFilter; format?: PixelFormat } = {}
-) => {
-  const tex = new DataTexture(array, width, height, format, FloatType)
-  tex.minFilter = tex.magFilter = filter
-  tex.wrapS = tex.wrapT = ClampToEdgeWrapping
-  return tex
-}
+export function useControlPoints(builder: GroupBuilder<any>) {
+  const data = useMemo(() => {
+    const curvePositionTex = new StorageTexture(
+      builder.settings.maxPoints,
+      builder.settings.maxCurves
+    )
+    curvePositionTex.type = FloatType
+    const curveColorTex = new StorageTexture(
+      builder.settings.maxPoints,
+      builder.settings.maxCurves
+    )
 
-export function packToTexture(
-  dimensions: Vector2,
-  curves: GroupBuilder<any>['curves']
-) {
-  const width = dimensions.x
-  const height = dimensions.y
+    const controlPointCounts = uniformArray(
+      builder.curves.map(x => x.length),
+      'int'
+    )
 
-  const positions = new Float32Array(
-    range(height).flatMap(i => {
-      const c = curves[i]
-      return c
-        ? range(width).flatMap(i => {
-            const point = c[i]
-            return point
-              ? [point.x, point.y, point.strength, point.thickness]
-              : [-1111, 0, 0, 0]
-          })
-        : range(width).flatMap(() => [-1111, 0, 0, 0])
-    })
+    const positionTex = new DataTexture(
+      new Float32Array(
+        builder.settings.maxPoints * builder.settings.maxCurves * 4
+      ),
+      builder.settings.maxPoints,
+      builder.settings.maxCurves,
+      RGBAFormat,
+      FloatType,
+      undefined,
+      undefined,
+      undefined,
+      NearestFilter,
+      NearestFilter
+    )
+    const colorTex = new DataTexture(
+      new Float32Array(
+        builder.settings.maxPoints * builder.settings.maxCurves * 4
+      ),
+      builder.settings.maxPoints,
+      builder.settings.maxCurves,
+      RGBAFormat,
+      FloatType
+    )
+
+    const pointI = instanceIndex.modInt(builder.settings.maxPoints)
+    const curveI = instanceIndex.div(builder.settings.maxPoints)
+    const curvePositionLoadU = textureLoad(positionTex, vec2(pointI, curveI))
+    const curveColorLoadU = textureLoad(colorTex, vec2(pointI, curveI))
+    const lastCurvePositionLoadU = textureLoad(
+      positionTex,
+      vec2(pointI, curveI)
+    )
+    const lastCurveColorLoadU = textureLoad(colorTex, vec2(pointI, curveI))
+    const aspectRatio = screenSize.div(screenSize.x).toVar('screenSize')
+
+    const advanceControlPoints = Fn(() => {
+      const textureVector = vec2(
+        pointI.toFloat().div(controlPointCounts.element(curveI)),
+        curveI.toFloat().div(builder.settings.maxCurves)
+      )
+      const info = {
+        pointUV: textureVector,
+        aspectRatio: aspectRatio.y,
+        settings: builder.settings
+      }
+      const point = builder.settings.curveVert(vec4(curvePositionLoadU), {
+        ...info,
+        lastPosition: vec4(lastCurvePositionLoadU)
+      })
+      textureStore(curvePositionTex, vec2(pointI, curveI), point).toWriteOnly()
+
+      const color = builder.settings.curveFrag(vec4(curveColorLoadU), {
+        ...info,
+        lastColor: vec4(lastCurveColorLoadU)
+      })
+      return textureStore(
+        curveColorTex,
+        vec2(pointI, curveI),
+        color
+      ).toWriteOnly()
+    })().compute(
+      builder.settings.maxPoints * builder.settings.maxCurves,
+      undefined as any
+    )
+
+    return {
+      advanceControlPoints,
+      curvePositionLoadU,
+      curveColorLoadU,
+      lastCurvePositionLoadU,
+      lastCurveColorLoadU,
+      curvePositionTex,
+      curveColorTex,
+      controlPointCounts
+    }
+  }, [builder])
+
+  let updating: number
+
+  // @ts-ignore
+  const gl = useThree(({ gl }) => gl as WebGPURenderer)
+  const resolution = new Vector2()
+  gl.getDrawingBufferSize(resolution)
+
+  const reInitialize = () => {
+    builder.reInitialize(resolution)
+    for (let i = 0; i < builder.settings.maxCurves; i++) {
+      data.controlPointCounts.array[i] = builder.curves[i].length
+    }
+
+    data.lastCurvePositionLoadU.value = data.curvePositionLoadU.value
+    data.lastCurveColorLoadU.value = data.curveColorLoadU.value
+
+    const loadPositions = data.curvePositionLoadU.value.source.data
+      .data as Float32Array
+    const loadColors = data.curveColorLoadU.value.source.data
+      .data as Float32Array
+    for (let i = 0; i < builder.settings.maxCurves; i++) {
+      const curveIndex = i * 4 * builder.settings.maxPoints
+      for (let j = 0; j < builder.settings.maxPoints; j++) {
+        const point = builder.curves[i]?.[j]
+        if (point) {
+          loadPositions.set(
+            [point.x, point.y, point.strength, point.thickness],
+            curveIndex + j * 4
+          )
+          loadColors.set(
+            [point.color[0], point.color[1], point.color[2], point.alpha],
+            curveIndex + j * 4
+          )
+        } else {
+          loadPositions.set([0, 0, 0, 0], curveIndex + j * 4)
+          loadColors.set([0, 0, 0, 0], curveIndex + j * 4)
+        }
+      }
+    }
+
+    data.curvePositionLoadU.value.needsUpdate = true
+    data.curveColorLoadU.value.needsUpdate = true
+
+    gl.computeAsync(data.advanceControlPoints)
+  }
+
+  const nextTime = useRef<number>(builder.settings.start / 1000)
+
+  useFrame(state => {
+    if (state.clock.elapsedTime > nextTime.current) {
+      if (builder.settings.recalculate) {
+        const r = builder.settings.recalculate
+        nextTime.current =
+          typeof r === 'boolean'
+            ? nextTime.current + 1 / 60
+            : typeof r === 'number'
+              ? nextTime.current + r / 1000
+              : nextTime.current + r(nextTime.current * 1000) / 1000
+        reInitialize()
+      }
+    }
+  })
+
+  const update = () => {
+    gl.computeAsync(data.advanceControlPoints)
+    updating = requestAnimationFrame(update)
+  }
+
+  reInitialize()
+  useEffect(() => {
+    if (builder.settings.update) {
+      updating = requestAnimationFrame(update)
+    }
+    return () => {
+      cancelAnimationFrame(updating)
+    }
+  }, [builder])
+
+  const { curvePositionTex, curveColorTex, controlPointCounts } = data
+  const curvePositionTexU = texture(curvePositionTex)
+  const verticesPerCurve = Math.floor(
+    (builder.settings.maxLength * resolution.x) / (builder.settings.spacing * 5)
   )
-  // const positionTex = createTexture(pos, RGBAFormat, LinearFilter)
 
-  const colors = new Float32Array(
-    range(height).flatMap(i => {
-      const c = curves[i]
-      return c
-        ? range(width).flatMap(i => {
-            const point = c[i]
-            return point ? [...point.color, point.alpha] : [-1111, 0, 0, 0]
-          })
-        : range(width).flatMap(() => [-1111, 0, 0, 0])
+  const aspectRatio = screenSize.div(screenSize.x).toVar('screenSize')
+
+  const getBezier = (index, position, rotation, thickness, color) => {
+    const curveIndex = index.div(2).div(verticesPerCurve).toVar('curveIndex')
+    const controlPointsCount = controlPointCounts.element(curveIndex)
+    const subdivisions = select(
+      controlPointsCount.equal(2),
+      1,
+      controlPointsCount.sub(2)
+    ).toVar('subdivisions')
+    const t = vec2(
+      index
+        .div(2)
+        .toFloat()
+        .mod(verticesPerCurve)
+        .div(verticesPerCurve - 0.99)
+        .mul(subdivisions),
+      curveIndex
+    )
+
+    If(t.x.equal(-1), () => {
+      color.assign(vec4(0, 0, 0, 0))
+    }).Else(() => {
+      If(controlPointsCount.equal(2), () => {
+        const p0 = textureLoadFix(curvePositionTexU, ivec2(0, t.y)).xy
+        const p1 = textureLoadFix(curvePositionTexU, ivec2(1, t.y)).xy
+        const pointUV = vec2(t.x, t.y.div(builder.settings.maxCurves))
+        const info = {
+          pointUV,
+          aspectRatio: aspectRatio.y,
+          settings: builder.settings
+        }
+        const progressPoint = mix(p0, p1, t.x)
+
+        const textureVector = vec2(
+          t.x.add(0.5).div(builder.settings.maxPoints),
+          t.y.add(0.5).div(builder.settings.maxCurves)
+        )
+        color.assign(
+          builder.settings.pointFrag(
+            vec4(texture(curveColorTex, textureVector)),
+            info
+          )
+        )
+        thickness.assign(texture(curvePositionTex, textureVector).w)
+        position.assign(progressPoint)
+
+        rotation.assign(atan(p1.sub(p0).y, p1.sub(p0).x).add(PI2.mul(0.25)))
+      }).Else(() => {
+        const pointProgress = t.x
+        const p2 = textureLoadFix(
+          curvePositionTexU,
+          ivec2(pointProgress.add(2), t.y)
+        ).xy.toVar('p2')
+        const p0 = textureLoadFix(
+          curvePositionTexU,
+          ivec2(pointProgress, t.y)
+        ).xy.toVar('p0')
+        const p1 = textureLoadFix(
+          curvePositionTexU,
+          ivec2(pointProgress.add(1), t.y)
+        ).xy.toVar('p1')
+
+        If(pointProgress.greaterThan(float(1)), () => {
+          p0.assign(mix(p0, p1, float(0.5)))
+        })
+        If(pointProgress.lessThan(float(controlPointsCount).sub(3)), () => {
+          p2.assign(mix(p1, p2, 0.5))
+        })
+        const strength = textureLoadFix(
+          curvePositionTexU,
+          ivec2(pointProgress.add(1), t.y)
+        ).z.toVar('strength')
+        const thisPoint = bezierPoint({
+          t: pointProgress.fract(),
+          p0,
+          p1,
+          p2,
+          strength
+        })
+        const tt = vec2(
+          // 2 points: 0.5-1.5
+          pointProgress
+            .div(controlPointsCount.sub(2))
+            .mul(controlPointsCount.sub(1))
+            .add(0.5)
+            .div(builder.settings.maxPoints),
+          t.y.add(0.5).div(builder.settings.maxCurves)
+        )
+        const pointUV = vec2(
+          pointProgress.div(controlPointsCount.sub(2)),
+          t.y.div(builder.settings.maxCurves)
+        )
+        const info = {
+          pointUV,
+          aspectRatio: aspectRatio.y,
+          settings: builder.settings
+        }
+        color.assign(
+          builder.settings.pointFrag(vec4(texture(curveColorTex, tt)), info)
+        )
+        thickness.assign(texture(curvePositionTex, tt).w)
+        position.assign(builder.settings.pointVert(thisPoint.position, info))
+        rotation.assign(thisPoint.rotation)
+      })
     })
-  )
+    thickness.divAssign(screenSize.x)
+  }
 
-  return { positions, colors }
+  useEffect(() => {
+    return () => {
+      curvePositionTex.dispose()
+      curveColorTex.dispose()
+    }
+  }, [])
+
+  return {
+    getBezier
+  }
 }
