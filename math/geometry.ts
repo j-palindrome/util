db6ee840f7259076447e0d05e831f53f7dc86a25
Jay Reinier <jtreinier@gmail.com>
2025-02-08 21:57:36 -0500

    update packages

--- math/geometry.ts
+++ math/geometry.ts
@@ -1,4 +1,36 @@
 import { Vector2 } from 'three'
+// export const lerpVectors = glsl `
+// vec2 lerpVectors(vec2 a, vec2 b, float lerp) {
+//   return a + (b - a) * lerp;
+// }`
+
+const bezierJS = (t: number, p0: Vector2, p1: Vector2, p2: Vector2) => {
+  const tInverse = 1 - t
+  const test = new Vector2()
+  const test2 = new Vector2()
+  return test
+    .copy(p0)
+    .multiplyScalar(tInverse ** 2)
+    .add(test2.copy(p1).multiplyScalar(2 * tInverse * t))
+    .add(test2.copy(p2).multiplyScalar(t ** 2))
+}
+
+export const multiBezierJS = (t: number, ...points: Vector2[]) => {
+  const subdivisions = points.length - 2
+  const progress = t * subdivisions // 0 -> numSubdivisions
+  const startCurve = Math.floor(progress)
+  const p1 = points[startCurve + 1]
+  const p0 =
+    startCurve > 1
+      ? points[startCurve].clone().lerp(p1, 0.5)
+      : points[startCurve]
+  const p2 =
+    startCurve === points.length - 3
+      ? points[startCurve + 2].clone().lerp(p1, 0.5)
+      : points[startCurve + 2]
+  return bezierJS(progress % 1, p0, p1, p2)
+}
+
 export const bezier2JS = (
   t: number,
   p0: [number, number],
--- webgl/formattings.ts
+++ webgl/formattings.ts
@@ -1,9 +1,4 @@
-import _, { range } from 'lodash'
 import { rotate2d } from './manipulation'
-import { rad } from '../math/math'
-import { PI } from './utilities'
-import { PointBuilder } from '../asemic/PointBuilder'
-import { Vector2 } from 'three'
 
 export const arc = /*glsl*/ `
 vec2 arc(float t, vec2 center, float r) {
@@ -111,38 +106,6 @@ vec2 polyLine(float t, vec2 p0, vec2 p1, vec2 p2) {
   }
 }
 `
-
-// export const lerpVectors = glsl `
-// vec2 lerpVectors(vec2 a, vec2 b, float lerp) {
-//   return a + (b - a) * lerp;
-// }`
-
-const bezierJS = (t: number, p0: Vector2, p1: Vector2, p2: Vector2) => {
-  const tInverse = 1 - t
-  const test = new Vector2()
-  const test2 = new Vector2()
-  return test
-    .copy(p0)
-    .multiplyScalar(tInverse ** 2)
-    .add(test2.copy(p1).multiplyScalar(2 * tInverse * t))
-    .add(test2.copy(p2).multiplyScalar(t ** 2))
-}
-
-export const multiBezierJS = (t: number, ...points: Vector2[]) => {
-  const subdivisions = points.length - 2
-  const progress = t * subdivisions // 0 -> numSubdivisions
-  const startCurve = Math.floor(progress)
-  const p1 = points[startCurve + 1]
-  const p0 =
-    startCurve > 1
-      ? points[startCurve].clone().lerp(p1, 0.5)
-      : points[startCurve]
-  const p2 =
-    startCurve === points.length - 3
-      ? points[startCurve + 2].clone().lerp(p1, 0.5)
-      : points[startCurve + 2]
-  return bezierJS(progress % 1, p0, p1, p2)
-}
 
 export const multiBezierProgress = /*glsl*/ `
 vec2 multiBezierProgress (float t, int numPoints) {
